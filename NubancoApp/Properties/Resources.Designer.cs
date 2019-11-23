779e3266696900ee8d2ca8c9272ef392fbe5d0ae
Luan Vieira <38441055+LuanVsouza@users.noreply.github.com>
2019-11-23 15:22:45 -0300

    Login
    Login

--- NubancoApp/Properties/Resources.Designer.cs
+++ NubancoApp/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace NubancoApp.Properties {
     // através de uma ferramenta como ResGen ou Visual Studio.
     // Para adicionar ou remover um associado, edite o arquivo .ResX e execute ResGen novamente
     // com a opção /str, ou recrie o projeto do VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -57,6 +57,16 @@ namespace NubancoApp.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo_XPEvents1 {
+            get {
+                object obj = ResourceManager.GetObject("Logo-XPEvents1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
